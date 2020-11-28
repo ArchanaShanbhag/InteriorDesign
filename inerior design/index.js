385f4944641717865bb4ccec3fc3636fdb1a5896
ArchanaShanbhag <71385986+ArchanaShanbhag@users.noreply.github.com>
2020-11-28 11:05:15 +0530

    Add files via upload

---
+++ inerior design/index.js
@@ -0,0 +1,82 @@
+const menu = document.querySelector(".menu");
+const navOpen = document.querySelector(".hamburger");
+const navClose = document.querySelector(".close");
+
+const navLeft = menu.getBoundingClientRect().left;
+navOpen.addEventListener("click", () => {
+  if (navLeft < 0) {
+    menu.classList.add("show");
+  }
+});
+
+navClose.addEventListener("click", () => {
+  if (navLeft < 0) {
+    menu.classList.remove("show");
+  }
+});
+
+// Fixed Nav
+const nav = document.querySelector(".nav");
+const navHeight = nav.getBoundingClientRect().height;
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > navHeight) {
+    nav.classList.add("fix-nav");
+  } else {
+    nav.classList.remove("fix-nav");
+  }
+});
+
+// Scroll To
+const links = [...document.querySelectorAll(".scroll-link")];
+links.map(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    const fixNav = nav.classList.contains("fix-nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixNav) {
+      position = position;
+    }
+
+    window.scrollTo({
+      top: position,
+      left: 0,
+    });
+
+    menu.classList.remove("show");
+  });
+});
+
+AOS.init();
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
